e6744b14f2e53e2305b7b32f2ce32b09d82d5201
alptugozates <omeralptugozates@gmail.com>
2023-11-25 14:35:33 +0300

    provider ile app'i wrapler, importları yapar

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
+const store = createStore(reducer, applyMiddleware(thunk, logger))
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 
 root.render(
   <BrowserRouter>
-    <>
+    <Provider store={store}>
       <App />
-    </>
+    </Provider>
   </BrowserRouter>
 );
